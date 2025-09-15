3798b34bce03df6c41073d2fd70b8a35cf10e7f7
Aytaj Gahramanova <132328862+AytajGahramanova@users.noreply.github.com>
2025-09-15 12:52:41 +0400

    fix: vite.config

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -10,5 +10,5 @@ export default defineConfig({
       sans: ["Poppins", "sans-serif"],
     },
   },
-  base: process.env.VITE_BASE_PATH || "/soudemy",
+  base: import.meta.env.VITE_BASE_PATH || "/soudemy",
 });
